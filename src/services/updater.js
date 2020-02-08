9f0e0bd3252a0c5430998bb100dc0954de0d5f60
Andrey Yartsev <andrey.yartsev.g@gmail.com>
2020-02-08 13:58:46 +0300

    hotfix for updater

--- src/services/updater.js
+++ src/services/updater.js
@@ -6,9 +6,13 @@ module.exports = async (models) => {
     pages: 3
   });
   await afishaNnovUpdater.run();
-  setInterval(afishaNnovUpdater.run, 1000 * 60 * 10);
+  setInterval(() => {
+    afishaNnovUpdater.run();
+  }, 1000 * 60 * 10);
 
   const borzostUpdater = new BorzostUpdater(models);
   await borzostUpdater.run();
-  setInterval(borzostUpdater.run, 1000 * 60 * 60 * 3);
+  setInterval(() => {
+    borzostUpdater.run();
+  }, 1000 * 60 * 60 * 3);
 };
