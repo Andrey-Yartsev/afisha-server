6aae934ec329fb07bd51c13e22935000d4a08f72
Andrey Yartsev <andrey.yartsev.g@gmail.com>
2022-02-15 09:13:22 +0300

    Add auth logic

--- src/routes/admin/auth.js
+++ src/routes/admin/auth.js
@@ -1,15 +1,16 @@
+const Joi = require('@hapi/joi');
+
 module.exports = [
   {
     method: 'POST',
     path: '/api/admin/login',
-    handler: (req) => {
-      console.log(req.payload);
-      if (req.payload.password !== process.env.ADMIN_PASS) {
-        return { error: "Wrong pass" };
+    handler: (request) => {
+      if (request.payload.password !== process.env.ADMIN_PASS) {
+         return { error: "Wrong pass" };
       }
       return { success: true };
     },
-    config: {
+    options: {
       description: 'Login',
       tags: ['admin']
     }
