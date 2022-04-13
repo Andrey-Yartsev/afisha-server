ce6f4a100439bb973310f42522a1608cdffc9c64
Andrey Yartsev <andrey.yartsev.g@gmail.com>
2022-04-13 14:10:42 +0300

    hotfix

--- src/services/updater.js
+++ src/services/updater.js
@@ -3,27 +3,27 @@ const DvizhEventsUpdater = require("../lib/events/DvizhEventsUpdater");
 const EventsUpdater = require("../lib/events/EventsUpdater");
 
 module.exports = async (models) => {
-  // const afishaNnovUpdater = new AfishaNnovUpdater(models, {
-  //   pages: 4,
-  //   // useOnlyPage: 2,
-  //   // useOnlyI: 4,
-  //   // outputTextOnError: true
-  // });
-  // afishaNnovUpdater.run();
-  // setInterval(() => {
-  //    afishaNnovUpdater.run();
-  // }, 1000 * 60 * 10);
+  const afishaNnovUpdater = new AfishaNnovUpdater(models, {
+    pages: 4,
+    // useOnlyPage: 2,
+    // useOnlyI: 4,
+    // outputTextOnError: true
+  });
+  afishaNnovUpdater.run();
+  setInterval(() => {
+     afishaNnovUpdater.run();
+  }, 1000 * 60 * 10);
+
+  const dvizhUpdater = new DvizhEventsUpdater(models);
+  dvizhUpdater.run();
+  setInterval(() => {
+    dvizhUpdater.run();
+  }, 1000 * 60 * 60);
+
   //
-  // const dvizhUpdater = new DvizhEventsUpdater(models);
-  // dvizhUpdater.run();
-  // setInterval(() => {
-  //   dvizhUpdater.run();
-  // }, 1000 * 60 * 60);
-
-
-  new EventsUpdater('nenavist_club', models, new VkEventsParser({
-    vkGroupName: 'nenavist_club'
-  }));
+  // new EventsUpdater('nenavist_club', models, new VkEventsParser({
+  //   vkGroupName: 'nenavist_club'
+  // }));
 
 
 };
