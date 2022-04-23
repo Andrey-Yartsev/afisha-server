95742e54bb5d5f307289955aa980f3d55b84e365
Andrey Yartsev <andrey.yartsev.g@gmail.com>
2022-04-23 08:56:58 +0300

    improve afisha parser

--- src/services/updater.js
+++ src/services/updater.js
@@ -5,25 +5,25 @@ const EventsUpdater = require("../lib/events/updater/EventsUpdater");
 module.exports = async (models) => {
   const afishaNnovUpdater = new AfishaNnovUpdater(models, {
     pages: 4,
-    useOnlyPage: 3,
-    useOnlyI: 0,
-    outputTextOnError: true
+    // useOnlyPage: 2,
+    // useOnlyI: 4,
+    // outputTextOnError: true
   });
   afishaNnovUpdater.run();
-  // setInterval(() => {
-  //    afishaNnovUpdater.run();
-  // }, 1000 * 60 * 10);
+  setInterval(() => {
+     afishaNnovUpdater.run();
+  }, 1000 * 60 * 10);
+
+  const dvizhUpdater = new DvizhEventsUpdater(models);
+  dvizhUpdater.run();
+  setInterval(() => {
+    dvizhUpdater.run();
+  }, 1000 * 60 * 60);
+
   //
-  // const dvizhUpdater = new DvizhEventsUpdater(models);
-  // dvizhUpdater.run();
-  // setInterval(() => {
-  //   dvizhUpdater.run();
-  // }, 1000 * 60 * 60);
-  //
-  // //
-  // // new EventsUpdater('nenavist_club', models, new VkEventsParser({
-  // //   vkGroupName: 'nenavist_club'
-  // // }));
+  // new EventsUpdater('nenavist_club', models, new VkEventsParser({
+  //   vkGroupName: 'nenavist_club'
+  // }));
 
 
 };
