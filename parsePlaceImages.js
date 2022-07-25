8c21cc600a781f5ba4741a07d126bfe07538b356
Andrey Yartsev <andrey.yartsev.g@gmail.com>
2022-07-25 15:29:00 +0300

    uprovement for place image parse

--- parsePlaceImages.js
+++ parsePlaceImages.js
@@ -12,25 +12,33 @@ global.appRoot = path.resolve(__dirname);
 (async () => {
 
   const updateImageRecord = async (models, placeId) => {
-      await models.PlaceImage.updateOne(
-        {placeId},
-        {
-          $set: {
-            placeId,
-            dtUpdate: Date.now()
-          },
+    await models.PlaceImage.updateOne(
+      {placeId},
+      {
+        $set: {
+          placeId,
+          dtUpdate: Date.now()
         },
-        {upsert: true},
-      );
-    };
-
+      },
+      {upsert: true},
+    );
+  };
   const models = await db();
+
+  let placeImages = await models.PlaceImage.find({});//.map(v => v.placeId);
+  let existingPlaceIds = placeImages.map(placeImage => placeImage.placeId);
+
   let places = require(global.appRoot + '/src/lib/events/parser/places/places.js');
   const sliceFrom = 0;
   // const sliceCount = 1;
   // places = places.splice(sliceFrom, sliceCount);
   let placeId = sliceFrom + 1;
   for await (let place of places) {
+    if (existingPlaceIds.indexOf(placeId) !== 0) {
+      console.log('exists: ', placeId);
+      placeId++;
+      continue;
+    }
     if (!place.vk) {
       console.log('place has no vk ', place);
       placeId++;
